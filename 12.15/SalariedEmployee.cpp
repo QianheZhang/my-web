f71baa52f2595a5277ca3938bf4e84b4addf7017
QianheZhang <48266076+QianheZhang@users.noreply.github.com>
2019-06-09 11:52:15 +0800

    no message

---
+++ 12.15/SalariedEmployee.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdexcept>
+#include "SalariedEmployee.h"
+
+using namespace std;
+
+SalariedEmployee::SalariedEmployee(const string &first,
+                                   const string &last,const string &ssn,double salary)
+                                   :Employee(first,last,ssn)
+{
+   setWeeklySalary(salary);
+}
+
+void SalariedEmployee::SetWeeklySalary(double salary)
+{
+    if (salary>=0.0)
+        weeklySalary=salary;
+    else
+        throw invalid_argument("Weekly salary must be >=0.0");
+}
+
+double SalariedEmployee::getWeeklySalary()const
+{
+    return weeklySalary;
+}
+
+double SalariedEmployee::earnings()const
+{
+    return getWeeklySalary();
+}
+
